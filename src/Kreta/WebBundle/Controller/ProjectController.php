5eb082794c813350dfe4876dd5e8893b33daa1b9
Gorka Laucirica <gorka.lauzirika@gmail.com>
2014-10-17 16:47:44 +0200

    Add participants to a project

--- src/Kreta/WebBundle/Controller/ProjectController.php
+++ src/Kreta/WebBundle/Controller/ProjectController.php
@@ -12,7 +12,6 @@
 namespace Kreta\WebBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ProjectController extends Controller
 {
@@ -21,9 +20,32 @@ class ProjectController extends Controller
         $project = $this->get('kreta_core.repository_project')->find($id);
 
         if (!$project) {
-            throw new NotFoundHttpException();
+            throw $this->createNotFoundException('Project not found');
         }
 
         return $this->render('KretaWebBundle:Project:view.html.twig', array('project' => $project));
+    }
+
+    function newParticipantAction($id)
+    {
+        /** @var \Kreta\Component\Core\Model\Interfaces\UserInterface $user */
+        $user = $this->get('kreta_core.repository_user')->findOneBy(array('email' => $this->getRequest()->get('email')));
+        /** @var \Kreta\Component\Core\Model\Interfaces\ProjectInterface $project */
+        $project = $this->get('kreta_core.repository_project')->find($id);
+
+        if(!$user) {
+            throw $this->createNotFoundException('User not found');
+        }
+
+        if(!$project) {
+            throw $this->createNotFoundException('Project not found');
+        }
+
+        $project->addParticipant($user);
+        $manager = $this->getDoctrine()->getManager();
+        $manager->persist($project);
+        $manager->flush();
+
+        return $this->redirect($this->generateUrl('kreta_web_project_view', array('id' => $project->getId())));
     }
 }
