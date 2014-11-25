e63aa099d573d150f19ed1b717bd6a0996cdb34a
benatespina <benatespina@gmail.com>
2014-11-25 01:50:52 +0100

    Added image preview in project and user forms

---
+++ src/Kreta/Bundle/WebBundle/Resources/public/js/uploader.js
@@ -0,0 +1,22 @@
+/**
+ * This file belongs to Kreta.
+ * The source code of application includes a LICENSE file
+ * with all information about license.
+ *
+ * @author benatespina <benatespina@gmail.com>
+ * @author gorkalaucirica <gorka.lauzirika@gmail.com>
+ */
+
+$(document).ready(function () {
+    var $photo, $image;
+
+    $photo = $('#kreta_core_user_type_photo');
+    $image = $('#kreta_core_project_type_image');
+
+    $photo.on('change', function () {
+        $('.kreta-image-preview')[0].src = window.URL.createObjectURL(this.files[0])
+    });
+    $image.on('change', function() {
+        $('.kreta-image-preview')[0].src = window.URL.createObjectURL(this.files[0])
+    });
+});
