1cbbef1b93f05cb266cc0e4387031b2052850051
benatespina <benatespina@gmail.com>
2014-12-08 17:15:26 +0100

    Added Behat support for project api methods

---
+++ src/Kreta/Bundle/Api/ApiCoreBundle/Behat/OauthContext.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * This file belongs to Kreta.
+ * The source code of application includes a LICENSE file
+ * with all information about license.
+ *
+ * @author benatespina <benatespina@gmail.com>
+ * @author gorkalaucirica <gorka.lauzirika@gmail.com>
+ */
+
+namespace Kreta\Bundle\Api\ApiCoreBundle\Behat;
+
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\TableNode;
+use Behat\Symfony2Extension\Context\KernelAwareContext;
+use Behat\Symfony2Extension\Context\KernelDictionary;
+use Behat\WebApiExtension\Context\WebApiContext;
+
+/**
+ * Class OauthContext.
+ *
+ * @package Kreta\Bundle\Api\ApiCoreBundle\Behat
+ */
+class OauthContext extends WebApiContext implements Context, KernelAwareContext
+{
+    use KernelDictionary;
+
+    /**
+     * Populates the database with a client and access tokens.
+     *
+     * @param \Behat\Gherkin\Node\TableNode $tokens The tokens
+     *
+     * @return void
+     *
+     * @Given /^the following tokens exist:$/
+     */
+    public function theFollowingTokensExist(TableNode $tokens)
+    {
+        $container = $this->kernel->getContainer();
+        $manager = $container->get('doctrine')->getManager();
+
+        $client = $container->get('kreta_api_core.command_createClientCommand')
+            ->generateClient(['http://kreta.io'], ['password']);
+
+        $accessTokenManager = $container->get('fos_oauth_server.access_token_manager.default');
+        foreach ($tokens as $token) {
+            $accessToken = $accessTokenManager->createToken();
+            $accessToken->setClient($client);
+            $accessToken->setToken($token['token']);
+            $accessToken->setExpiresAt($token['expiresAt']);
+            $accessToken->setScope($token['scope']);
+            
+            $user = $container->get('kreta_core.repository_user')->findOneBy(['email' => $token['user']]);
+            $accessToken->setUser($user);
+
+            $manager->persist($accessToken);
+        }
+
+        $manager->flush();
+    }
+
+    /**
+     * Adds OAuth2 Authentication header to next request.
+     *
+     * @param string $accessToken The access token
+     *
+     * @return void
+     *
+     * @Given /^I am authenticating with "([^"]*)" token/
+     */
+    public function iAmAuthenticatingWithToken($accessToken)
+    {
+        $this->removeHeader('Authorization');
+        $this->addHeader('Authorization', 'Bearer ' . $accessToken);
+    }
+}
---
+++ src/Kreta/Bundle/Api/ApiCoreBundle/Behat/ProjectContext.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file belongs to Kreta.
+ * The source code of application includes a LICENSE file
+ * with all information about license.
+ *
+ * @author benatespina <benatespina@gmail.com>
+ * @author gorkalaucirica <gorka.lauzirika@gmail.com>
+ */
+
+namespace Kreta\Bundle\Api\ApiCoreBundle\Behat;
+
+use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\RawMinkContext;
+use Behat\Symfony2Extension\Context\KernelAwareContext;
+use Behat\Symfony2Extension\Context\KernelDictionary;
+
+/**
+ * Class ProjectContext.
+ *
+ * @package Kreta\Bundle\Api\ApiCoreBundle\Behat
+ */
+class ProjectContext extends RawMinkContext implements Context, KernelAwareContext
+{
+    use KernelDictionary;
+
+    /**
+     * @Given /^the following projects exist:$/
+     */
+    public function theFollowingProjectsExist(TableNode $projects)
+    {
+        $manager = $this->kernel->getContainer()->get('doctrine')->getManager();
+
+        foreach($projects as $projectData) {
+            $project = $this->kernel->getContainer()->get('kreta_core.factory_project')->create();
+            $project->setId($projectData['id']);
+            $project->setName($projectData['name']);
+            $project->setShortName($projectData['shortName']);
+
+            $metadata = $manager->getClassMetaData(get_class($project));
+            $metadata->setIdGenerator(new \Doctrine\ORM\Id\AssignedGenerator());
+
+            $manager->persist($project);
+        }
+
+        $manager->flush();
+    }
+}
