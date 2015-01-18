f3c92de0903bdd13b318c8a5d5b0201d78c2e96d
Gorka Laucirica <gorka.lauzirika@gmail.com>
2015-01-18 21:18:51 +0100

    Removed unused use statement

--- src/Kreta/Bundle/NotificationBundle/DependencyInjection/KretaNotificationExtension.php
+++ src/Kreta/Bundle/NotificationBundle/DependencyInjection/KretaNotificationExtension.php
@@ -12,7 +12,6 @@
 namespace Kreta\Bundle\NotificationBundle\DependencyInjection;
 
 use Kreta\Bundle\CoreBundle\DependencyInjection\Abstracts\AbstractExtension;
-use Symfony\Component\DependencyInjection\Loader;
 
 /**
  * Class KretaNotificationExtension.
