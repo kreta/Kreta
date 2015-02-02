d6b66b378ff772c76dc2825bdaacb61f7f44a8b8
Asier <uthopiko@gmail.com>
2015-02-02 10:01:26 +0100

    Improvement for dynamic injection of services

---
+++ src/Kreta/Bundle/CoreBundle/DependencyInjection/Extension.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file belongs to Kreta.
+ * The source code of application includes a LICENSE file
+ * with all information about license.
+ *
+ * @author benatespina <benatespina@gmail.com>
+ * @author gorkalaucirica <gorka.lauzirika@gmail.com>
+ */
+
+namespace Kreta\Bundle\CoreBundle\DependencyInjection;
+
+use Kreta\Bundle\CoreBundle\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension as BaseExtension;
+
+/**
+ * Class Extension.
+ *
+ * @package Kreta\Bundle\CoreBundle\DependencyInjection
+ */
+class Extension extends BaseExtension
+{
+    /**
+     * Gets the path for configuration directories.
+     *
+     * @return string
+     */
+    public function getConfigurationDirectoryPath()
+    {
+        $classPath = new \ReflectionClass($this);
+        $nameSpaceDir = dirname($classPath->getFileName());
+        return $nameSpaceDir . '/../Resources/config';
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $this->processConfiguration($configuration, $configs);
+        $path = $this->getConfigurationDirectoryPath();
+        $loader = new YamlFileLoader($container, new FileLocator($path));
+        $loader->loadFilesFromDirectory($path);
+    }
+}
---
+++ src/Kreta/Bundle/CoreBundle/DependencyInjection/Loader/YamlFileLoader.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * This file belongs to Kreta.
+ * The source code of application includes a LICENSE file
+ * with all information about license.
+ *
+ * @author benatespina <benatespina@gmail.com>
+ * @author gorkalaucirica <gorka.lauzirika@gmail.com>
+ */
+
+namespace Kreta\Bundle\CoreBundle\DependencyInjection\Loader;
+
+use Symfony\Component\Yaml\Parser;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader as BaseYamlFileLoader;
+
+/**
+ * Class YamlFileLoader.
+ *
+ * @package Kreta\Bundle\CoreBundle\DependencyInjection\Loader
+ */
+class YamlFileLoader extends BaseYamlFileLoader
+{
+    /**
+     * Load all YAML Files from directory.
+     *
+     * @param string $path Path for resources
+     *
+     * @return null
+     */
+    public function loadFilesFromDirectory($path)
+    {
+        if ($handle = opendir($path)) {
+            while (false !== ($file = readdir($handle))) {
+                if (strpos($file, '.yml')) {
+                    if ($this->existInFile($path . '/' . $file, 'services')) {
+                        $this->load($file);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Analyze content of YAML for search term.
+     *
+     * @param string $file Path for search content
+     * @param string $searchTerm Searched term in file
+     *
+     * @return boolean
+     */
+    protected function existInFile($file, $searchTerm)
+    {
+        $parser = new Parser();
+        $parsedResult = $parser->parse(file_get_contents($file));
+        return array_key_exists($searchTerm, $parsedResult);
+    }
+}
