9509df8619f955081a97fa3083210d05d22d1f61
Gorka Laucirica <gorka.lauzirika@gmail.com>
2015-02-07 13:52:42 +0100

    Definition of TimeTrackingBundle's basic structure

---
+++ src/Kreta/Bundle/TimeTrackingBundle/DependencyInjection/KretaTimeTrackingExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file belongs to Kreta.
+ * The source code of application includes a LICENSE file
+ * with all information about license.
+ *
+ * @author benatespina <benatespina@gmail.com>
+ * @author gorkalaucirica <gorka.lauzirika@gmail.com>
+ */
+
+namespace Kreta\Bundle\TimeTrackingBundle\DependencyInjection;
+
+use Kreta\Bundle\CoreBundle\DependencyInjection\Extension;
+
+/**
+ * Class KretaTimeTrackingExtension.
+ *
+ * @package Kreta\Bundle\TimeTrackingBundle\DependencyInjection
+ */
+class KretaTimeTrackingExtension extends Extension
+{
+}
---
+++ src/Kreta/Bundle/TimeTrackingBundle/KretaTimeTrackingBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file belongs to Kreta.
+ * The source code of application includes a LICENSE file
+ * with all information about license.
+ *
+ * @author benatespina <benatespina@gmail.com>
+ * @author gorkalaucirica <gorka.lauzirika@gmail.com>
+ */
+
+namespace Kreta\Bundle\TimeTrackingBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class KretaTimeTrackingBundle.
+ *
+ * @package Kreta\TimeTrackingBundle
+ */
+class KretaTimeTrackingBundle extends Bundle
+{
+}
