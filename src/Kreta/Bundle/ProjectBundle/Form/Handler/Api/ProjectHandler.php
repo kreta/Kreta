51302da15e3731bdf02a80d2f5ccd5f0140a6af5
benatespina <benatespina@gmail.com>
2015-03-17 23:03:08 +0100

    Initial commit about refactoring the forms
    - Create AbstractType over Symfony's AbstractType
    - Build ProjectType as a service

--- src/Kreta/Bundle/ProjectBundle/Form/Handler/Api/ProjectHandler.php
+++ src/Kreta/Bundle/ProjectBundle/Form/Handler/Api/ProjectHandler.php
@@ -11,18 +11,9 @@
 
 namespace Kreta\Bundle\ProjectBundle\Form\Handler\Api;
 
-use Doctrine\Common\Persistence\ObjectManager;
-use Kreta\Bundle\ProjectBundle\Form\Type\Api\ProjectType;
 use Kreta\Bundle\ProjectBundle\Form\Handler\ProjectHandler as BaseProjectFormHandler;
-use Kreta\Component\Media\Factory\MediaFactory;
-use Kreta\Component\Media\Uploader\MediaUploader;
-use Kreta\Component\Project\Factory\ProjectFactory;
-use Kreta\Component\Workflow\Repository\WorkflowRepository;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\Form\FormFactory;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\FileBag;
-use Symfony\Component\Security\Core\SecurityContextInterface;
 
 /**
  * Class ProjectHandler.
@@ -32,63 +23,14 @@ use Symfony\Component\Security\Core\SecurityContextInterface;
 class ProjectHandler extends BaseProjectFormHandler
 {
     /**
-     * The context.
-     *
-     * @var \Symfony\Component\Security\Core\SecurityContextInterface
-     */
-    protected $context;
-
-    /**
-     * The project factory.
-     *
-     * @var \Kreta\Component\Project\Factory\ProjectFactory
-     */
-    protected $factory;
-
-    /**
-     * The workflow repository.
-     *
-     * @var \Kreta\Component\Workflow\Repository\WorkflowRepository
-     */
-    protected $workflowRepository;
-
-    /**
-     * Constructor.
-     *
-     * @param \Symfony\Component\Form\FormFactory                         $formFactory        The form factory
-     * @param \Doctrine\Common\Persistence\ObjectManager                  $manager            The manager
-     * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $eventDispatcher    The event dispatcher
-     * @param \Kreta\Component\Media\Factory\MediaFactory                 $mediaFactory       The media factory
-     * @param \Kreta\Component\Media\Uploader\MediaUploader               $uploader           Uploads Project images
-     * @param \Symfony\Component\Security\Core\SecurityContextInterface   $context            The security context
-     * @param \Kreta\Component\Project\Factory\ProjectFactory             $projectFactory     The project factory
-     * @param \Kreta\Component\Workflow\Repository\WorkflowRepository     $workflowRepository The workflow repository
-     */
-    public function __construct(
-        FormFactory $formFactory,
-        ObjectManager $manager,
-        EventDispatcherInterface $eventDispatcher,
-        MediaFactory $mediaFactory,
-        MediaUploader $uploader,
-        SecurityContextInterface $context,
-        ProjectFactory $projectFactory,
-        WorkflowRepository $workflowRepository
-    )
-    {
-        parent::__construct($formFactory, $manager, $eventDispatcher, $mediaFactory, $uploader);
-        $this->context = $context;
-        $this->factory = $projectFactory;
-        $this->workflowRepository = $workflowRepository;
-    }
-
-    /**
      * {@inheritdoc}
      */
     protected function createForm($object = null, array $formOptions = [])
     {
-        return $this->formFactory->create(
-            new ProjectType($this->context, $this->factory, $this->workflowRepository), $object, $formOptions
-        );
+        return $this->formFactory->createNamedBuilder(
+            '', 'kreta_project_project_type', $object, $formOptions
+        )->getForm();
+
     }
 
     /**
--- src/Kreta/Bundle/ProjectBundle/Form/Type/Api/ProjectType.php
+++ src/Kreta/Bundle/ProjectBundle/Form/Type/Api/ProjectType.php
@@ -11,68 +11,29 @@
 
 namespace Kreta\Bundle\ProjectBundle\Form\Type\Api;
 
-use Kreta\Bundle\ProjectBundle\Form\Type\ProjectType as BaseProjectType;
-use Kreta\Component\Project\Factory\ProjectFactory;
-use Kreta\Component\User\Model\Interfaces\UserInterface;
-use Kreta\Component\Workflow\Repository\WorkflowRepository;
+use Kreta\Bundle\CoreBundle\Form\Type\Abstracts\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Symfony\Component\Security\Core\SecurityContextInterface;
 
 /**
  * Class ProjectType.
  *
  * @package Kreta\Bundle\ProjectBundle\Form\Type\Api
  */
-class ProjectType extends BaseProjectType
+class ProjectType extends AbstractType
 {
-    /**
-     * The context.
-     *
-     * @var \Symfony\Component\Security\Core\SecurityContextInterface
-     */
-    protected $context;
-
-    /**
-     * The project factory.
-     *
-     * @var \Kreta\Component\Project\Factory\ProjectFactory
-     */
-    protected $factory;
-
-    /**
-     * The workflow repository.
-     *
-     * @var \Kreta\Component\Workflow\Repository\WorkflowRepository
-     */
-    protected $workflowRepository;
-
-    /**
-     * Constructor.
-     *
-     * @param \Symfony\Component\Security\Core\SecurityContextInterface $context            The context
-     * @param \Kreta\Component\Project\Factory\ProjectFactory           $projectFactory     The project factory
-     * @param \Kreta\Component\Workflow\Repository\WorkflowRepository   $workflowRepository The workflow repository
-     */
-    public function __construct(
-        SecurityContextInterface $context,
-        ProjectFactory $projectFactory,
-        WorkflowRepository $workflowRepository
-    )
-    {
-        $this->context = $context;
-        $this->factory = $projectFactory;
-        $this->workflowRepository = $workflowRepository;
-    }
-
     /**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        parent::buildForm($builder, $options);
         $builder
+            ->add('name')
+            ->add('shortName')
+            ->add('image', 'file', [
+                'required' => false,
+                'mapped' => false
+            ])
             ->add('workflow', 'entity', [
                 'class'    => 'Kreta\Component\Workflow\Model\Workflow',
                 'required' => false,
@@ -83,23 +44,12 @@ class ProjectType extends BaseProjectType
     /**
      * {@inheritdoc}
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    protected function createEmptyData(FormInterface $form)
     {
-        $resolver->setDefaults([
-            'data_class'      => 'Kreta\Component\Project\Model\Project',
-            'csrf_protection' => false,
-            'empty_data'      => function (FormInterface $form) {
-                $user = $this->context->getToken()->getUser();
-                if (!($user instanceof UserInterface)) {
-                    throw new \Exception('User is not logged');
-                }
+        $workflowId = $form->get('workflow')->getData() ? $form->get('workflow')->getData() : 'non-exist-id';
+        $workflow = $this->manager->getRepository('Kreta\Component\Workflow\Model\Workflow')->find($workflowId);
 
-                $workflowId = $form->get('workflow')->getData() ? $form->get('workflow')->getData() : 'non-exist-id';
-                $workflow = $this->workflowRepository->find($workflowId);
-
-                return $this->factory->create($user, $workflow);
-            }
-        ]);
+        return $this->factory->create($this->user, $workflow);
     }
 
     /**
@@ -107,6 +57,6 @@ class ProjectType extends BaseProjectType
      */
     public function getName()
     {
-        return '';
+        return 'kreta_project_project_type';
     }
 }
