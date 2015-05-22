6387cd9e6855205bd44dcaf7de477cab306f560b
benatespina <benatespina@gmail.com>
2015-05-22 12:30:29 +0200

    Removed unnecessary dependency from MediaBundle

--- src/Kreta/Bundle/MediaBundle/spec/Kreta/Bundle/MediaBundle/EventSubscriber/MediaUrlSubscriberSpec.php
+++ src/Kreta/Bundle/MediaBundle/spec/Kreta/Bundle/MediaBundle/EventSubscriber/MediaUrlSubscriberSpec.php
@@ -12,6 +12,7 @@
 namespace spec\Kreta\Bundle\MediaBundle\EventSubscriber;
 
 use JMS\Serializer\EventDispatcher\ObjectEvent;
+use Kreta\Bundle\MediaBundle\Stubs\ObjectStub;
 use Kreta\Component\Media\Model\Interfaces\MediaInterface;
 use Kreta\Component\Project\Model\Project;
 use PhpSpec\ObjectBehavior;
@@ -27,7 +28,7 @@ class MediaUrlSubscriberSpec extends ObjectBehavior
 {
     function let(Router $router)
     {
-        $this->beConstructedWith($router, 'Kreta\Component\Project\Model\Project', 'image');
+        $this->beConstructedWith($router, 'Kreta\Bundle\MediaBundle\Stubs\ObjectStub', 'image');
     }
 
     function it_is_initializable()
@@ -54,30 +55,30 @@ class MediaUrlSubscriberSpec extends ObjectBehavior
 
     function it_does_not_change_object_media_because_the_object_does_not_be_a_class_instance(ObjectEvent $event)
     {
-        $event->getObject()->shouldBeCalled()->willReturn(Argument::not('Kreta\Component\Project\Model\Project'));
+        $event->getObject()->shouldBeCalled()->willReturn(Argument::not('Kreta\Bundle\MediaBundle\Stubs\ObjectStub'));
 
         $this->onChangeObjectMedia($event);
     }
 
     function it_does_not_change_object_media_because_the_image_does_not_be_an_media(
         ObjectEvent $event,
-        Project $project
+        ObjectStub $object
     )
     {
-        $event->getObject()->shouldBeCalled()->willReturn($project);
-        $project->getImage()->shouldBeCalled()->willReturn(Argument::not('Kreta\Component\Core\Model\Media'));
+        $event->getObject()->shouldBeCalled()->willReturn($object);
+        $object->getImage()->shouldBeCalled()->willReturn(Argument::not('Kreta\Component\Core\Model\Media'));
 
         $this->onChangeObjectMedia($event);
     }
 
     function it_does_not_change_object_media_because_the_media_already_has_a_href_as_name(
         ObjectEvent $event,
-        Project $project,
+        ObjectStub $object,
         MediaInterface $media
     )
     {
-        $event->getObject()->shouldBeCalled()->willReturn($project);
-        $project->getImage()->shouldBeCalled()->willReturn($media);
+        $event->getObject()->shouldBeCalled()->willReturn($object);
+        $object->getImage()->shouldBeCalled()->willReturn($media);
         $media->getName()->shouldBeCalled()->willReturn('http://kreta.io/media/media/media-name.jpg');
 
         $this->onChangeObjectMedia($event);
@@ -85,13 +86,13 @@ class MediaUrlSubscriberSpec extends ObjectBehavior
 
     function it_changes_media_name(
         ObjectEvent $event,
-        Project $project,
+        ObjectStub $object,
         MediaInterface $media,
         Router $router
     )
     {
-        $event->getObject()->shouldBeCalled()->willReturn($project);
-        $project->getImage()->shouldBeCalled()->willReturn($media);
+        $event->getObject()->shouldBeCalled()->willReturn($object);
+        $object->getImage()->shouldBeCalled()->willReturn($media);
 
         $media->getName()->shouldBeCalled()->willReturn('media-name.jpg');
 
@@ -99,7 +100,7 @@ class MediaUrlSubscriberSpec extends ObjectBehavior
             ->shouldBeCalled()->willReturn('http://kreta.io/media/media/media-name.jpg');
 
         $media->setName('http://kreta.io/media/media/media-name.jpg')->shouldBeCalled()->willReturn($media);
-        $project->setImage($media)->shouldBeCalled()->willReturn($project);
+        $object->setImage($media)->shouldBeCalled()->willReturn($object);
 
         $this->onChangeObjectMedia($event);
     }
