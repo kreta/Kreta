d1958ef011408db743f1137541f821fabc501be6
benatespina <benatespina@gmail.com>
2015-10-05 17:24:51 +0200

    Built all the js stack with webpack

--- Bundle/WebBundle/webpack.config.babel.js
+++ Bundle/WebBundle/webpack.config.babel.js
@@ -1,54 +1,62 @@
+/*
+ * This file belongs to Kreta.
+ * The source code of application includes a LICENSE file
+ * with all information about license.
+ *
+ * @author benatespina <benatespina@gmail.com>
+ * @author gorkalaucirica <gorka.lauzirika@gmail.com>
+ */
+
+import minimist from 'minimist';
 import path from 'path';
 import webpack from 'webpack';
 
-const config = {
+import pkg from './package.json';
+
+const cliOptions = minimist(process.argv),
+  LICENSE = `${pkg.name} - ${pkg.description}
+Authors: ${pkg.authors[0].name} - ${pkg.authors[1].name}
+Url: ${pkg.homepage}
+License: ${pkg.license}`;
+
+let config = {
+  debug: false,
+  devtool: 'eval',
   entry: {
     appJS: path.resolve(__dirname, 'Resources/public/js/kreta.js'),
     vendorsJS: [
-      'jquery',
-      'lodash',
-      'backbone',
-      'backbone-model-file-upload',
-      'backbone.marionette',
-      'select2',
-      'mousetrap'
+      'jquery', 'lodash', 'backbone', 'backbone-model-file-upload', 'backbone.marionette', 'select2', 'mousetrap'
     ]
   },
   output: {
-    path: path.resolve(__dirname, '../../../../web/js'),
-    filename: 'kreta.js'
+    path: path.resolve(__dirname, '../../../../web/js'), filename: 'kreta.js'
   },
   module: {
-    preLoaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loaders: [
-        'eslint-loader'
-      ]
-    }],
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel'
-    }]
+    preLoaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['eslint-loader']}
+    ],
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']}
+    ]
   },
   resolve: {
-    alias: {
-      underscore: 'lodash'
-    }
+    alias: {underscore: 'lodash'}
   },
-  eslint: {
-    configFile: '.eslint.yml'
-  },
+  eslint: {configFile: '.eslint.yml'},
   plugins: [
     new webpack.ProvidePlugin({
-      _: 'lodash',
-      Backbone: 'backbone',
-      $: 'jquery',
-      jQuery: 'jquery'
+      _: 'lodash', Backbone: 'backbone', $: 'jquery', jQuery: 'jquery'
     }),
-    new webpack.optimize.CommonsChunkPlugin('vendorsJS', 'vendors.js')
+    new webpack.optimize.CommonsChunkPlugin('vendorsJS', 'vendors.js'),
+    new webpack.BannerPlugin(LICENSE)
   ]
 };
+
+if (cliOptions.hasOwnProperty('env') && cliOptions.env === 'prod') {
+  config.debug = false;
+  config.devtool = '#source-map';
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+  config.output.filename = 'kreta.min.js';
+}
 
 export default config;
