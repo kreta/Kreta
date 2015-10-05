a067a47067183d5e3c7b03f2eafdd0c33a9ee4dc
benatespina <benatespina@gmail.com>
2015-10-05 20:11:34 +0200

    More tasks with webpack

--- Bundle/WebBundle/webpack.config.babel.js
+++ Bundle/WebBundle/webpack.config.babel.js
@@ -7,6 +7,8 @@
  * @author gorkalaucirica <gorka.lauzirika@gmail.com>
  */
 
+import autoprefixer from 'autoprefixer';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import minimist from 'minimist';
 import path from 'path';
 import webpack from 'webpack';
@@ -23,7 +25,9 @@ let config = {
   debug: false,
   devtool: 'eval',
   entry: {
-    appJS: path.resolve(__dirname, 'Resources/public/js/kreta.js'),
+    js: path.resolve(__dirname, 'Resources/public/js/kreta.js'),
+    app: path.resolve(__dirname, 'Resources/public/scss/app.scss'),
+    login: path.resolve(__dirname, 'Resources/public/scss/login.scss'),
     vendorsJS: [
       'jquery', 'lodash', 'backbone', 'backbone-model-file-upload', 'backbone.marionette', 'select2', 'mousetrap'
     ]
@@ -36,27 +40,35 @@ let config = {
       {test: /\.js$/, exclude: /node_modules/, loaders: ['eslint-loader']}
     ],
     loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']}
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
+      {test: /\.(jpe?g|png|gif|svg)$/, loader: 'file-loader'},
+      {test: /\.scss$/, loader: ExtractTextPlugin.extract('style-loader!scss-lint-loader!css-loader!postcss!sass-loader')}
     ]
   },
   resolve: {
     alias: {underscore: 'lodash'}
   },
   eslint: {configFile: '.eslint.yml'},
+  postcss: [autoprefixer({browsers: ['last 2 version']})],
   plugins: [
     new webpack.ProvidePlugin({
       _: 'lodash', Backbone: 'backbone', $: 'jquery', jQuery: 'jquery'
     }),
     new webpack.optimize.CommonsChunkPlugin('vendorsJS', 'vendors.js'),
-    new webpack.BannerPlugin(LICENSE)
+    new webpack.BannerPlugin(LICENSE),
+    new ExtractTextPlugin('../css/[name].css', {allChunks: false})
   ]
 };
 
 if (cliOptions.hasOwnProperty('env') && cliOptions.env === 'prod') {
   config.debug = false;
-  config.devtool = '#source-map';
+  config.devtool = 'source-map';
   config.plugins.push(new webpack.optimize.UglifyJsPlugin());
   config.output.filename = 'kreta.min.js';
+  config.module.loaders.push({
+    test: /\.css$/,
+    loader: 'style-loader/useable!css-loader?minimize!postcss-loader'
+  });
 }
 
 export default config;
