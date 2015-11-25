e30f5626e99b1da98e04f2430831ba501b78e7b0
benatespina <benatespina@gmail.com>
2015-11-25 18:36:42 +0100

    First commit about Flux

---
+++ src/Kreta/Bundle/WebBundle/Resources/public/js/Dispatcher/AppDispatcher.js
@@ -0,0 +1,24 @@
+/*
+ * This file is part of the Kreta package.
+ *
+ * (c) Beñat Espiña <benatespina@gmail.com>
+ * (c) Gorka Laucirica <gorka.lauzirika@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import {Dispatcher} from 'flux';
+
+class DispatcherClass extends Dispatcher {
+  handleViewAction(action) {
+    this.dispatch({
+      source: 'VIEW_ACTION',
+      action: action
+    });
+  }
+}
+
+const AppDispatcher = new DispatcherClass();
+
+export default AppDispatcher;
---
+++ src/Kreta/Bundle/WebBundle/Resources/public/js/constants/AppConstants.js
@@ -0,0 +1,20 @@
+/*
+ * This file is part of the Kreta package.
+ *
+ * (c) Beñat Espiña <benatespina@gmail.com>
+ * (c) Gorka Laucirica <gorka.lauzirika@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+const AppConstants = {
+  NEW_ISSUE: 'NEW_ISSUE',
+  SAVE_ISSUE: 'SAVE_ISSUE',
+  NEW_PROJECT: 'NEW_PROJECT',
+  SAVE_PROJECT: 'SAVE_PROJECT',
+  UPDATE_PROFILE: 'UPDATE_PROFILE',
+  SAVE_PROFILE: 'SAVE_PROFILE'
+};
+
+export default AppConstants;
