e1360b353e823aa9844524c17220ea73a4eb4af8
Gorka Laucirica <gorka.lauzirika@gmail.com>
2016-01-17 14:03:02 +0100

    Initial flux approach for Issue New

---
+++ src/Kreta/Bundle/WebBundle/Resources/public/js/constants/ActionTypes.js
@@ -0,0 +1,26 @@
+/*
+ * This file is part of the Kreta package.
+ *
+ * (c) Beñat Espiña <benatespina@gmail.com>
+ * (c) Gorka Laucirica <gorka.lauzirika@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+const ActionTypes = {
+  CHANGE_EVENT: 'CHANGE_EVENT',
+
+  ISSUE_LIST: 'ISSUE_LIST',
+  ISSUE_LIST_RECEIVED: 'ISSUE_LIST_RECEIVED',
+  ISSUE_CREATE: 'ISSUE_CREATE',
+  ISSUE_CREATED: 'ISSUE_CREATED',
+  ISSUE_CREATE_ERROR: 'ISSUE_CREATE_ERROR',
+  ISSUE_UPDATE: 'ISSUE_UPDATE',
+  ISSUE_UPDATED: 'ISSUE_UPDATED',
+
+  PROFILE_UPDATE: 'PROFILE_UPDATE',
+  PROFILE_UPDATED: 'PROFILE_UPDATED'
+};
+
+export default ActionTypes;
--- src/Kreta/Bundle/WebBundle/Resources/public/js/util/ApiIssueUtils.js
+++ src/Kreta/Bundle/WebBundle/Resources/public/js/util/ApiIssueUtils.js
@@ -8,13 +8,10 @@
  * file that was distributed with this source code.
  */
 
-const AppConstants = {
-  NEW_ISSUE: 'NEW_ISSUE',
-  SAVE_ISSUE: 'SAVE_ISSUE',
-  NEW_PROJECT: 'NEW_PROJECT',
-  SAVE_PROJECT: 'SAVE_PROJECT',
-  UPDATE_PROFILE: 'UPDATE_PROFILE',
-  SAVE_PROFILE: 'SAVE_PROFILE'
-};
+import {issueCreated} from '../actions/IssueActionCreator';
 
-export default AppConstants;
+export function createIssue(issue) {
+  setTimeout(() => {
+    issueCreated(issue);
+  }, 100)
+}
