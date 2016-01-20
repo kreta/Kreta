e3602adfe7df28a7f13c04a6ff3d196ff7c51c5f
benatespina <benatespina@gmail.com>
2016-01-20 09:29:36 +0100

    Trying...

--- src/Kreta/Bundle/CoreBundle/Behat/Context/WebApiContext.php
+++ src/Kreta/Bundle/CoreBundle/Behat/Context/WebApiContext.php
@@ -97,8 +97,7 @@ class WebApiContext implements ApiClientAwareContext
      */
     public function iSendARequest($method, $url)
     {
-        $url = $this->prepareUrl('http://kreta.test:8000/app.php' . $url);
-        echo $url;
+        $url = $this->prepareUrl('http://kreta.test:8000' . $url);
         $this->request = $this->getClient()->createRequest($method, $url);
         if (!empty($this->headers)) {
             $this->request->addHeaders($this->headers);
