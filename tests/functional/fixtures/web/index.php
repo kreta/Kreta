5225662f4bddf77269df92f79e472b20e76d8b24
benatespina <benatespina@gmail.com>
2016-01-21 18:33:30 +0100

    Trying...

--- tests/functional/fixtures/web/index.php
+++ tests/functional/fixtures/web/index.php
@@ -19,14 +19,6 @@ date_default_timezone_set('Europe/Madrid');
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1']) || php_sapi_name() === 'cli-server')
-) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
-}
-
 $loader = require __DIR__ . '/../app/autoload.php';
 Debug::enable();
 
