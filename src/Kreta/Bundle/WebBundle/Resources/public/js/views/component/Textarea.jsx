73fe8a2908355eb9e05b59ba77244737fa1d4f30
benatespina <benatespina@gmail.com>
2016-01-23 17:52:31 +0100

    Added minor styles for better integration of alloyeditor

--- src/Kreta/Bundle/WebBundle/Resources/public/js/views/component/Textarea.jsx
+++ src/Kreta/Bundle/WebBundle/Resources/public/js/views/component/Textarea.jsx
@@ -15,13 +15,41 @@ import React from 'react';
 
 class Textarea extends React.Component {
   static propTypes = {
-//    configuration: React.PropTypes.string.isRequired,
     content: React.PropTypes.string,
     id: React.PropTypes.string.isRequired
   };
 
   componentDidMount() {
-    this._editor = AlloyEditor.editable(this.props.id, this.props.alloyEditorConfig);
+    this._editor = AlloyEditor.editable(this.props.id, {
+      toolbars: {
+        add: {
+          buttons: ['image', 'hline', 'table'],
+          tabIndex: 2
+        },
+        styles: {
+          selections: [{
+            name: 'link',
+            buttons: ['linkEdit'],
+            test: AlloyEditor.SelectionTest.link
+          }, {
+            name: 'image',
+            buttons: ['imageLeft', 'imageRight'],
+            test: AlloyEditor.SelectionTest.image
+          }, {
+            name: 'text',
+            buttons: ['styles', 'bold', 'italic', 'underline', 'link'],
+            test: AlloyEditor.SelectionTest.text
+          }, {
+            name: 'table',
+            buttons: ['tableRow', 'tableColumn', 'tableCell', 'tableRemove'],
+            getArrowBoxClasses: AlloyEditor.SelectionGetArrowBoxClasses.table,
+            setPosition: AlloyEditor.SelectionSetPosition.table,
+            test: AlloyEditor.SelectionTest.table
+          }],
+          tabIndex: 1
+        }
+      }
+    });
   }
 
   componentWillUnmount() {
