4b912b1490d4361e175094aae8eec58c9879d121
gorkalaucirica <gorka.lauzirika@gmail.com>
2016-01-29 19:37:12 +0100

    Simulate issue creation

--- src/Kreta/Bundle/WebBundle/Resources/public/js/reducers/CurrentProject.js
+++ src/Kreta/Bundle/WebBundle/Resources/public/js/reducers/CurrentProject.js
@@ -17,6 +17,9 @@ export default function reducer(state = initialState, action = {}) {
     case ActionTypes.CURRENT_PROJECT_RECEIVED:
       return {...state, project: action.project, issues: action.issues, fetching: false};
 
+    case ActionTypes.CURRENT_PROJECT_ISSUE_CREATED:
+      return {...state, issues: [...state.issues, action.issue]};
+
     case ActionTypes.CURRENT_PROJECT_SELECTED_ISSUE_CHANGED:
       return {...state, selectedIssue: action.selectedIssue};
     default:
