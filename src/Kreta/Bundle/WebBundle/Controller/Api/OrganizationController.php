2cbd8a67d717d08b898d6bc6363041f8cd662799
benatespina <benatespina@gmail.com>
2016-02-10 18:27:55 +0100

    Added api private endpoint for better front-end development

---
+++ src/Kreta/Bundle/WebBundle/Controller/Api/OrganizationController.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ * This file is part of the Kreta package.
+ *
+ * (c) Beñat Espiña <benatespina@gmail.com>
+ * (c) Gorka Laucirica <gorka.lauzirika@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Kreta\Bundle\WebBundle\Controller\Api;
+
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Kreta\Bundle\OrganizationBundle\Security\Authorization\Voter\OrganizationVoter;
+use Kreta\Component\Organization\Model\Interfaces\OrganizationInterface;
+use Kreta\Component\Project\Model\Interfaces\ProjectInterface;
+use Kreta\SimpleApiDocBundle\Annotation\ApiDoc;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+/**
+ * Organization private api controller class.
+ *
+ * @author Beñat Espiña <benatespina@gmail.com>
+ */
+class OrganizationController extends Controller
+{
+    /**
+     * Returns the organization for given slug.
+     *
+     * @param string $organizationSlug The slug of organization
+     *
+     * @ApiDoc(statusCodes={200, 403, 404})
+     * @Rest\View(statusCode=200, serializerGroups={"organization"})
+     * @Rest\Get("/organizations/{organizationSlug}")
+     *
+     * @return OrganizationInterface
+     */
+    public function getOrganizationAction($organizationSlug)
+    {
+        $organization = $this->get('kreta_organization.repository.organization')->findOneBy(
+            ['slug' => $organizationSlug], false
+        );
+        if (!$this->get('security.authorization_checker')->isGranted(OrganizationVoter::VIEW, $organization)) {
+            throw new AccessDeniedException();
+        }
+
+        return $organization;
+    }
+
+    /**
+     * Returns the projects for given organization slug.
+     *
+     * @param string $organizationSlug The slug of organization
+     *
+     * @ApiDoc(statusCodes={200, 403, 404})
+     * @Rest\View(statusCode=200, serializerGroups={"projectList"})
+     * @Rest\Get("/organizations/{organizationSlug}/projects")
+     *
+     * @return ProjectInterface[]
+     */
+    public function getOrganizationProjectsAction($organizationSlug)
+    {
+        $organization = $this->get('kreta_organization.repository.organization')->findOneBy(
+            ['slug' => $organizationSlug], false
+        );
+        if (!$this->get('security.authorization_checker')->isGranted(OrganizationVoter::VIEW, $organization)) {
+            throw new AccessDeniedException();
+        }
+
+        return $organization->getProjects();
+    }
+}
---
+++ src/Kreta/Bundle/WebBundle/Resources/public/js/api/ApiPrivate.js
@@ -0,0 +1,31 @@
+/*
+ * This file is part of the Kreta package.
+ *
+ * (c) Beñat Espiña <benatespina@gmail.com>
+ * (c) Gorka Laucirica <gorka.lauzirika@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import Api from './Api';
+import Config from './../Config';
+
+class OrganizationApiPrivate extends Api {
+  baseUrl() {
+    return `${Config.baseUrl}/private`;
+  }
+
+  getOrganization(organizationSlug, query = null) {
+    return this.get(`/organizations/${organizationSlug}`, query);
+  }
+}
+
+class ProjectApiPrivate extends Api {
+  getProjectsByOrganizations(organizationSlug, query = null) {
+    return this.get(`/organizations/${organizationSlug}/projects`, query);
+  }
+}
+
+export const organizationApiPrivateInstance = new OrganizationApiPrivate();
+export const projectApiPrivateInstance = new ProjectApiPrivate();
