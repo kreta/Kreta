d68d48868263a68d66486773115cb8205d489bba
Gorka Laucirica <gorka.lauzirika@gmail.com>
2016-10-09 22:26:30 +0200

    Added missing headers after CS script

--- src/Kreta/SharedKernel/Tests/Spec/Kreta/SharedKernel/Domain/Model/Identity/UsernameSpec.php
+++ src/Kreta/SharedKernel/Tests/Spec/Kreta/SharedKernel/Domain/Model/Identity/UsernameSpec.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of the Kreta package.
+ *
+ * (c) Beñat Espiña <benatespina@gmail.com>
+ * (c) Gorka Laucirica <gorka.lauzirika@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Spec\Kreta\SharedKernel\Domain\Model\Identity;
 
 use Kreta\SharedKernel\Domain\Model\Identity\Username;
