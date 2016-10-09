adc3eb956f02faf9f71d601cec302f43ae7e1123
Gorka Laucirica <gorka.lauzirika@gmail.com>
2016-10-09 22:27:41 +0200

    Added task aggregate specs

--- src/Kreta/TaskManager/Tests/Spec/Kreta/TaskManager/Domain/Model/Project/Task/TaskSpec.php
+++ src/Kreta/TaskManager/Tests/Spec/Kreta/TaskManager/Domain/Model/Project/Task/TaskSpec.php
@@ -18,20 +18,112 @@ use PhpSpec\ObjectBehavior;
 
 class TaskSpec extends ObjectBehavior
 {
-    function let(TaskId $taskId)
+    function let(TaskId $taskId, ProjectMemberId $creator, ProjectMemberId $assignee)
     {
         $taskId->id()->willReturn('task-id');
-        $this->beConstructedWith($taskId);
+        $this->beConstructedWith($taskId, 'Title', 'Description', $creator, $assignee, Task::PRIORITY_LOW);
     }
 
-    function it_is_initializable()
+    function it_can_be_created(TaskId $taskId, ProjectMemberId $creator, ProjectMemberId $assignee)
     {
         $this->shouldHaveType(Task::class);
+        $this->id()->shouldReturn($taskId);
+        $this->title()->shouldReturn('Title');
+        $this->description()->shouldReturn('Description');
+        $this->creator()->shouldReturn($creator);
+        $this->assignee()->shouldReturn($assignee);
+        $this->priority()->shouldReturn(Task::PRIORITY_LOW);
+        $this->parentId()->shouldReturn(null);
+
+        $this->shouldBeTodo();
+        $this->shouldNotBeDoing();
+        $this->shouldNotBeDone();
+
+        $this->__toString()->shouldReturn('task-id');
     }
 
-    function it_gets_id()
+    function it_can_be_created_as_a_subtask(TaskId $taskId,
+                                            ProjectMemberId $creator,
+                                            ProjectMemberId $assignee,
+                                            TaskId $parentId)
     {
-        $this->id()->shouldReturnAnInstanceOf(TaskId::class);
+        $this->beConstructedWith($taskId, 'Title', 'Description', $creator, $assignee, Task::PRIORITY_LOW, $parentId);
+        $this->shouldHaveType(Task::class);
+        $this->id()->shouldReturn($taskId);
+        $this->title()->shouldReturn('Title');
+        $this->description()->shouldReturn('Description');
+        $this->creator()->shouldReturn($creator);
+        $this->assignee()->shouldReturn($assignee);
+        $this->priority()->shouldReturn(Task::PRIORITY_LOW);
+        $this->parentId()->shouldReturn($parentId);
+
+        $this->shouldBeTodo();
+        $this->shouldNotBeDoing();
+        $this->shouldNotBeDone();
+
+        $this->shouldHavePublished(TaskCreated::class);
+
         $this->__toString()->shouldReturn('task-id');
+    }
+
+    function it_can_be_edited()
+    {
+        $this->edit('New title', 'New description');
+
+        $this->title()->shouldReturn('New title');
+        $this->description()->shouldReturn('New description');
+
+        $this->shouldHavePublished(TaskEdited::class);
+    }
+
+    function it_can_be_started()
+    {
+        $this->start();
+
+        $this->shouldNotBeTodo();
+        $this->shouldBeDoing();
+        $this->shouldNotBeDone();
+
+        $this->shouldHavePublished(TaskStarted::class);
+    }
+
+    function it_can_be_stopped()
+    {
+        $this->stop();
+
+        $this->shouldBeTodo();
+        $this->shouldNotBeDoing();
+        $this->shouldNotBeDone();
+
+        $this->shouldHavePublished(TaskStopped::class);
+    }
+
+    function it_can_be_finished()
+    {
+        $this->finish();
+
+        $this->shouldNotBeTodo();
+        $this->shouldNotBeDoing();
+        $this->shouldBeDone();
+
+        $this->shouldHavePublished(TaskFinished::class);
+    }
+
+    function it_can_be_reassigned(ProjectMemberId $assignee)
+    {
+        $this->reassign($assignee);
+
+        $this->assignee()->shouldReturn($assignee);
+
+        $this->shouldHavePublished(TaskReassigned::class);
+    }
+
+    function its_priority_can_be_changed()
+    {
+        $this->changePriority(Task::PRIORITY_HIGH);
+
+        $this->priority()->shouldReturn(Task::PRIORITY_HIGH);
+
+        $this->shouldHavePublished(TaskPriorityChanged::class);
     }
 }
