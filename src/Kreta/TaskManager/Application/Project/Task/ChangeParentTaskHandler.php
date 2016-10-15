dd21591f206c02692feac547ab2ba1f0db538cdf
benatespina <benatespina@gmail.com>
2016-10-15 14:39:08 +0200

    Added change parent task application service

---
+++ src/Kreta/TaskManager/Application/Project/Task/ChangeParentTaskHandler.php
@@ -0,0 +1,97 @@
+<?php
+
+/*
+ * This file is part of the Kreta package.
+ *
+ * (c) Beñat Espiña <benatespina@gmail.com>
+ * (c) Gorka Laucirica <gorka.lauzirika@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Kreta\TaskManager\Application\Project\Task;
+
+use Kreta\TaskManager\Domain\Model\Organization\MemberId;
+use Kreta\TaskManager\Domain\Model\Organization\OrganizationRepository;
+use Kreta\TaskManager\Domain\Model\Project\ProjectRepository;
+use Kreta\TaskManager\Domain\Model\Project\Task\Task;
+use Kreta\TaskManager\Domain\Model\Project\Task\TaskAndTaskParentCannotBeTheSameException;
+use Kreta\TaskManager\Domain\Model\Project\Task\TaskCreationNotAllowedException;
+use Kreta\TaskManager\Domain\Model\Project\Task\TaskDoesNotExistException;
+use Kreta\TaskManager\Domain\Model\Project\Task\TaskId;
+use Kreta\TaskManager\Domain\Model\Project\Task\TaskParentCannotBelongToOtherProjectException;
+use Kreta\TaskManager\Domain\Model\Project\Task\TaskParentDoesNotExistException;
+use Kreta\TaskManager\Domain\Model\Project\Task\TaskRepository;
+use Kreta\TaskManager\Domain\Model\User\UserId;
+
+class ChangeParentTaskHandler
+{
+    private $repository;
+    private $projectRepository;
+    private $organizationRepository;
+
+    public function __construct(
+        TaskRepository $repository,
+        ProjectRepository $projectRepository,
+        OrganizationRepository $organizationRepository
+    ) {
+        $this->repository = $repository;
+        $this->projectRepository = $projectRepository;
+        $this->organizationRepository = $organizationRepository;
+    }
+
+    public function __invoke(ChangeParentTaskCommand $command)
+    {
+        $task = $this->repository->taskOfId(
+            TaskId::generate(
+                $command->id()
+            )
+        );
+        if (!$task instanceof Task) {
+            throw new TaskDoesNotExistException();
+        }
+
+        if (null !== $parentId = $command->parentId()) {
+            if ($parentId === $task->id()->id()) {
+                throw new TaskAndTaskParentCannotBeTheSameException();
+            }
+            $parent = $this->repository->taskOfId(
+                TaskId::generate(
+                    $parentId
+                )
+            );
+            if (!$parent instanceof Task) {
+                throw new TaskParentDoesNotExistException();
+            }
+            if (!$task->projectId()->equals($parent->projectId())) {
+                throw new TaskParentCannotBelongToOtherProjectException();
+            }
+        }
+
+        $project = $this->projectRepository->projectOfId(
+            $task->projectId()
+        );
+        $organization = $this->organizationRepository->organizationOfId(
+            $project->organizationId()
+        );
+        $changerId = MemberId::generate(
+            UserId::generate(
+                $command->changerId()
+            ),
+            $organization->id()
+        );
+
+        if (!$organization->isMember($changerId)) {
+            throw new TaskCreationNotAllowedException();
+        }
+
+        $task->changeParent(
+            !isset($parent) ? null : $parent->id()
+        );
+
+        $this->repository->persist($task);
+    }
+}
---
+++ src/Kreta/TaskManager/Tests/Spec/Kreta/TaskManager/Domain/Model/Project/Task/TaskParentChangedSpec.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the Kreta package.
+ *
+ * (c) Beñat Espiña <benatespina@gmail.com>
+ * (c) Gorka Laucirica <gorka.lauzirika@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Spec\Kreta\TaskManager\Domain\Model\Project\Task;
+
+use Kreta\SharedKernel\Domain\Model\DomainEvent;
+use Kreta\TaskManager\Domain\Model\Project\Task\TaskId;
+use Kreta\TaskManager\Domain\Model\Project\Task\TaskParentChanged;
+use PhpSpec\ObjectBehavior;
+
+class TaskParentChangedSpec extends ObjectBehavior
+{
+    function let(TaskId $taskId, TaskId $parentId)
+    {
+        $this->beConstructedWith($taskId, $parentId);
+    }
+
+    function it_creates_a_task_parent_changed_event(TaskId $taskId, TaskId $parentId)
+    {
+        $this->shouldHaveType(TaskParentChanged::class);
+        $this->shouldImplement(DomainEvent::class);
+
+        $this->id()->shouldReturn($taskId);
+        $this->parentId()->shouldReturn($parentId);
+        $this->occurredOn()->shouldReturnAnInstanceOf(\DateTimeInterface::class);
+    }
+
+    function it_creates_a_task_parent_changed_event_without_parent(TaskId $taskId)
+    {
+        $this->beConstructedWith($taskId);
+        $this->id()->shouldReturn($taskId);
+        $this->parentId()->shouldReturn(null);
+        $this->occurredOn()->shouldReturnAnInstanceOf(\DateTimeInterface::class);
+    }
+}
