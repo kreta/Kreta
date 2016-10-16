742fc778718e4f5507bf80ef2f83d2c42c3aebe0
benatespina <benatespina@gmail.com>
2016-10-16 16:53:40 +0200

    First approach adding the organization Doctrine mappings

---
+++ src/Kreta/TaskManager/Infrastructure/Persistence/Doctrine/DBAL/Organization/Types/OrganizationIdType.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the Kreta package.
+ *
+ * (c) Beñat Espiña <benatespina@gmail.com>
+ * (c) Gorka Laucirica <gorka.lauzirika@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Kreta\TaskManager\Infrastructure\Persistence\Doctrine\DBAL\Organization\Types;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\GuidType;
+use Kreta\TaskManager\Domain\Model\Organization\OrganizationId;
+
+class OrganizationIdType extends GuidType
+{
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if ($value instanceof OrganizationId) {
+            return $value->id();
+        }
+
+        return $value;
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        return OrganizationId::generate($value);
+    }
+
+    public function getName()
+    {
+        return 'organization_id';
+    }
+}
