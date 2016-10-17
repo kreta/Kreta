b1341b076996cc4e15b3fe207a3bc6a88262c085
benatespina <benatespina@gmail.com>
2016-10-17 17:04:40 +0200

    Added edit project application service

---
+++ src/Kreta/TaskManager/Application/Project/EditProjectHandler.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * This file is part of the Kreta package.
+ *
+ * (c) Beñat Espiña <benatespina@gmail.com>
+ * (c) Gorka Laucirica <gorka.lauzirika@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Kreta\TaskManager\Application\Project;
+
+use Kreta\SharedKernel\Domain\Model\Identity\Slug;
+use Kreta\TaskManager\Domain\Model\Organization\OrganizationRepository;
+use Kreta\TaskManager\Domain\Model\Organization\OwnerId;
+use Kreta\TaskManager\Domain\Model\Project\Project;
+use Kreta\TaskManager\Domain\Model\Project\ProjectDoesNotExistException;
+use Kreta\TaskManager\Domain\Model\Project\ProjectId;
+use Kreta\TaskManager\Domain\Model\Project\ProjectName;
+use Kreta\TaskManager\Domain\Model\Project\ProjectRepository;
+use Kreta\TaskManager\Domain\Model\Project\UnauthorizedProjectActionException;
+use Kreta\TaskManager\Domain\Model\User\UserId;
+
+class EditProjectHandler
+{
+    private $repository;
+    private $organizationRepository;
+
+    public function __construct(ProjectRepository $repository, OrganizationRepository $organizationRepository)
+    {
+        $this->repository = $repository;
+        $this->organizationRepository = $organizationRepository;
+    }
+
+    public function __invoke(EditProjectCommand $command)
+    {
+        $project = $this->repository->projectOfId(
+            ProjectId::generate($command->id())
+        );
+        if (!$project instanceof Project) {
+            throw new ProjectDoesNotExistException();
+        }
+
+        $organization = $this->organizationRepository->organizationOfId(
+            $project->organizationId()
+        );
+
+        $ownerId = OwnerId::generate(
+            UserId::generate(
+                $command->editorId()
+            ),
+            $organization->id()
+        );
+        if (!$organization->isOwner($ownerId)) {
+            throw new UnauthorizedProjectActionException();
+        }
+
+        $project->edit(
+            new ProjectName($command->name()),
+            new Slug(
+                null === $command->slug() ? $command->name() : $command->slug()
+            )
+        );
+
+        $this->repository->persist($project);
+    }
+}
