da773992703e387c9998075f3adde5ce6074cbba
benatespina <benatespina@gmail.com>
2016-12-14 22:41:49 +0100

    Registered task fixture cli command as a service and added missing task fields mapping

---
+++ TaskManager/src/Kreta/TaskManager/Infrastructure/Persistence/Doctrine/Migrations/Version20161214205121.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Kreta\TaskManager\Infrastructure\Persistence\Doctrine\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20161214205121 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE task ADD creator_id CHAR(36) NOT NULL COMMENT \'(DC2Type:organization_member_id)\', ADD assignee_id CHAR(36) NOT NULL COMMENT \'(DC2Type:organization_member_id)\'');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE task DROP creator_id, DROP assignee_id');
+    }
+}
