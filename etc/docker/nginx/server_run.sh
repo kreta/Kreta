51882672c6dc8bd1e3147b234835bd1a9af51ef8
gorkalaucirica <gorka.lauzirika@gmail.com>
2017-02-17 19:24:04 +0100

    Use SYMFONY_ENV to determine which app_* nginx has to serve

--- etc/docker/nginx/server_run.sh
+++ etc/docker/nginx/server_run.sh
@@ -1,4 +1,5 @@
+#!/usr/bin/env bash
 envsubst '${COMPOSITE_UI_HOST}' < /etc/nginx/sites-available/compositeui.template > /etc/nginx/sites-enabled/compositeui.conf
-envsubst '${TASK_MANAGER_HOST}' < /etc/nginx/sites-available/taskmanager.template > /etc/nginx/sites-enabled/taskmanager.conf
-envsubst '${IDENTITY_ACCESS_HOST}' < /etc/nginx/sites-available/identityaccess.template > /etc/nginx/sites-enabled/identityaccess.conf
+envsubst '${TASK_MANAGER_HOST} ${SYMFONY_ENV}' < /etc/nginx/sites-available/taskmanager.template > /etc/nginx/sites-enabled/taskmanager.conf
+envsubst '${IDENTITY_ACCESS_HOST} ${SYMFONY_ENV}' < /etc/nginx/sites-available/identityaccess.template > /etc/nginx/sites-enabled/identityaccess.conf
 nginx
