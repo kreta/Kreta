15257adbd8585de476080ae49713c1acb4af05d3
Gorka Laucirica <gorka.lauzirika@gmail.com>
2017-02-19 22:43:16 +0100

    Created content agnostic Card components to replace UserPreview and ResourcePreview

---
+++ CompositeUi/src/views/component/CardExtended.js
@@ -0,0 +1,52 @@
+/*
+ * This file is part of the Kreta package.
+ *
+ * (c) Beñat Espiña <benatespina@gmail.com>
+ * (c) Gorka Laucirica <gorka.lauzirika@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import './../../scss/components/_card-extended';
+
+import React from 'react';
+
+class CardExtended extends React.Component {
+  getThumbnail() {
+    const {thumbnail} = this.props;
+
+    if (thumbnail) {
+      return (
+        <div className="card-extended__thumbnail">
+          {thumbnail}
+        </div>
+      );
+    }
+
+    return '';
+  }
+
+  render() {
+    const {title, subtitle, children} = this.props;
+
+    return (
+      <div className="card-extended">
+        {this.getThumbnail()}
+        <div className="card-extended__container">
+            <span className="card-extended__header">
+              {title}
+            </span>
+          <span className="card-extended__subheader">
+              {subtitle}
+            </span>
+        </div>
+        <div className="card-extended__actions">
+          {children}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CardExtended;
