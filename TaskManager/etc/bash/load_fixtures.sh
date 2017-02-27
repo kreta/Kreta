2c61f7aab8499ed49ea9d3d27303b314d4c00cac
gorkalaucirica <gorka.lauzirika@gmail.com>
2017-02-27 19:37:09 +0100

    Fixed issues with the amount of project and task fixtures generated and added missing command to load_fixtures.sh

--- TaskManager/etc/bash/load_fixtures.sh
+++ TaskManager/etc/bash/load_fixtures.sh
@@ -5,3 +5,5 @@ sleep 2
 etc/bin/symfony-console kreta:task-manager:fixtures:organizations
 sleep 2
 etc/bin/symfony-console kreta:task-manager:fixtures:projects
+sleep 2
+etc/bin/symfony-console kreta:task-manager:fixtures:tasks
