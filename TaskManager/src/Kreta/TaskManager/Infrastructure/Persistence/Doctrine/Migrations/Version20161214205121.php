71569332112568dcc913a74d6f6b4724e000a456
benatespina <benatespina@gmail.com>
2017-03-07 19:34:33 +0100

    Fixed CS

--- TaskManager/src/Kreta/TaskManager/Infrastructure/Persistence/Doctrine/Migrations/Version20161214205121.php
+++ TaskManager/src/Kreta/TaskManager/Infrastructure/Persistence/Doctrine/Migrations/Version20161214205121.php
@@ -19,14 +19,14 @@ use Doctrine\DBAL\Schema\Schema;
 
 class Version20161214205121 extends AbstractMigration
 {
-    public function up(Schema $schema)
+    public function up(Schema $schema) : void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE task ADD creator_id CHAR(36) NOT NULL COMMENT \'(DC2Type:organization_member_id)\', ADD assignee_id CHAR(36) NOT NULL COMMENT \'(DC2Type:organization_member_id)\'');
     }
 
-    public function down(Schema $schema)
+    public function down(Schema $schema) : void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
--- TaskManager/src/Kreta/TaskManager/Infrastructure/Persistence/Doctrine/Migrations/Version20170307182355.php
+++ TaskManager/src/Kreta/TaskManager/Infrastructure/Persistence/Doctrine/Migrations/Version20170307182355.php
@@ -1,32 +1,33 @@
 <?php
 
+/*
+ * This file is part of the Kreta package.
+ *
+ * (c) Beñat Espiña <benatespina@gmail.com>
+ * (c) Gorka Laucirica <gorka.lauzirika@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 namespace Kreta\TaskManager\Infrastructure\Persistence\Doctrine\Migrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 class Version20170307182355 extends AbstractMigration
 {
-    /**
-     * @param Schema $schema
-     */
-    public function up(Schema $schema)
+    public function up(Schema $schema) : void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE task ADD numeric_id INT NOT NULL');
     }
 
-    /**
-     * @param Schema $schema
-     */
-    public function down(Schema $schema)
+    public function down(Schema $schema) : void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE task DROP numeric_id');
