cb1f5b28887f69768ce5d7feeec17b3b47c03edd
benatespina <benatespina@gmail.com>
2017-05-21 17:35:26 +0200

    Added Notification marked as read domain event

---
+++ Notifier/tests/Spec/Kreta/Notifier/Domain/Model/Notification/NotificationMarkedAsReadSpec.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Kreta package.
+ *
+ * (c) Beñat Espiña <benatespina@gmail.com>
+ * (c) Gorka Laucirica <gorka.lauzirika@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Spec\Kreta\Notifier\Domain\Model\Notification;
+
+use Kreta\Notifier\Domain\Model\Notification\NotificationMarkedAsRead;
+use Kreta\Notifier\Domain\Model\Notification\NotificationStatus;
+use Kreta\SharedKernel\Domain\Model\DomainEvent;
+use PhpSpec\ObjectBehavior;
+
+class NotificationMarkedAsReadSpec extends ObjectBehavior
+{
+    function it_should_be_created()
+    {
+        $this->shouldHaveType(NotificationMarkedAsRead::class);
+        $this->shouldImplement(DomainEvent::class);
+        $this->status()->shouldReturnAnInstanceOf(NotificationStatus::class);
+        $this->occurredOn()->shouldReturnAnInstanceOf(\DateTimeInterface::class);
+    }
+}
