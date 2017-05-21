33f0e4b6c65ee2f9e9f58af6d804c7d7ebc7cc5f
benatespina <benatespina@gmail.com>
2017-05-21 17:23:26 +0200

    Added Notification marked as unread domain event

---
+++ Notifier/src/Kreta/Notifier/Domain/Model/Notification/NotificationMarkedAsUnread.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Kreta package.
+ *
+ * (c) Beñat Espiña <benatespina@gmail.com>
+ * (c) Gorka Laucirica <gorka.lauzirika@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Kreta\Notifier\Domain\Model\Notification;
+
+use Kreta\SharedKernel\Domain\Model\DomainEvent;
+
+class NotificationMarkedAsUnread implements DomainEvent
+{
+    public function status()
+    {
+        return NotificationType::unread();
+    }
+
+    public function occurredOn() : \DateTimeInterface
+    {
+        return new \DateTimeImmutable();
+    }
+}
---
+++ Notifier/tests/Spec/Kreta/Notifier/Domain/Model/Notification/NotificationMarkedAsUnreadSpec.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Kreta package.
+ *
+ * (c) Beñat Espiña <benatespina@gmail.com>
+ * (c) Gorka Laucirica <gorka.lauzirika@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Spec\Kreta\Notifier\Domain\Model\Notification;
+
+use Kreta\Notifier\Domain\Model\Notification\NotificationMarkedAsUnread;
+use Kreta\Notifier\Domain\Model\Notification\NotificationType;
+use Kreta\SharedKernel\Domain\Model\DomainEvent;
+use PhpSpec\ObjectBehavior;
+
+class NotificationMarkedAsUnreadSpec extends ObjectBehavior
+{
+    function it_should_be_created()
+    {
+        $this->shouldHaveType(NotificationMarkedAsUnread::class);
+        $this->shouldImplement(DomainEvent::class);
+        $this->status()->shouldReturnAnInstanceOf(NotificationType::class);
+        $this->occurredOn()->shouldReturnAnInstanceOf(\DateTimeInterface::class);
+    }
+}
