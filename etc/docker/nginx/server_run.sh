5088fe4c7ce2fdb887ae4457ef618e06a873984d
Gorka Laucirica <gorka.lauzirika@gmail.com>
2017-06-06 20:40:47 +0200

    Added infraestructure required by notifier

--- etc/docker/nginx/server_run.sh
+++ etc/docker/nginx/server_run.sh
@@ -2,4 +2,5 @@
 envsubst '${COMPOSITE_UI_HOST}' < /etc/nginx/sites-available/compositeui.template > /etc/nginx/sites-enabled/compositeui.conf
 envsubst '${TASK_MANAGER_HOST} ${SYMFONY_ENV}' < /etc/nginx/sites-available/taskmanager.template > /etc/nginx/sites-enabled/taskmanager.conf
 envsubst '${IDENTITY_ACCESS_HOST} ${SYMFONY_ENV}' < /etc/nginx/sites-available/identityaccess.template > /etc/nginx/sites-enabled/identityaccess.conf
+envsubst '${NOTIFIER_HOST} ${SYMFONY_ENV}' < /etc/nginx/sites-available/notifier.template > /etc/nginx/sites-enabled/notifier.conf
 nginx
