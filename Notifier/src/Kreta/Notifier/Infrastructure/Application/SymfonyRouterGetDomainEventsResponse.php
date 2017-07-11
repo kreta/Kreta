2d072fedac958ed5435068035711f361c8a79585
benatespina <benatespina@gmail.com>
2017-07-11 18:30:08 +0200

    Improved _links with edge case pagination

--- Notifier/src/Kreta/Notifier/Infrastructure/Application/SymfonyRouterGetDomainEventsResponse.php
+++ Notifier/src/Kreta/Notifier/Infrastructure/Application/SymfonyRouterGetDomainEventsResponse.php
@@ -30,16 +30,35 @@ class SymfonyRouterGetDomainEventsResponse implements GetDomainEventsResponse
     {
         return [
             '_meta'  => [
-                'count' => count($events),
+                'count' => $this->numberOfEvents($events),
                 'page'  => $page,
             ],
-            '_links' => [
-                'first' => $this->urlGenerate(1),
-                'next'  => $this->urlGenerate($page + 1),
-                'self'  => $this->urlGenerate($page),
-            ],
+            '_links' => $this->links($events, $page, $pageSize),
             'data'   => $events,
         ];
+    }
+
+    private function numberOfEvents(array $events) : int
+    {
+        return count($events);
+    }
+
+    private function links(array $events, int $page, int $pageSize) : array
+    {
+        $links = [
+            'first' => $this->urlGenerate(1),
+            'self'  => $this->urlGenerate($page),
+        ];
+
+        if ($this->numberOfEvents($events) === $pageSize) {
+            $links = array_merge(
+                ['first' => $this->urlGenerate(1)],
+                ['self' => $links['self']],
+                ['next' => $this->urlGenerate($page + 1)]
+            );
+        }
+
+        return $links;
     }
 
     private function urlGenerate(int $page) : string
